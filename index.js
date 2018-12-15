cbb87d5ccf1beaaf38d81df4418acaece8a81322
Julia <juliaaugust96@gmail.com>
2018-12-16 00:43:18 +0300

    more pages

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ app.set("view engine", "ejs");
 
 app.listen(3000);
 
+app.use('/css', express.static('style'))
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/main/index.html");
+});
+
+app.get('/profile', function (req, res) {
+  var info = {curr_level: "2", solved_tasks: "5", publ_articles: "0"};
+  res.render('profile', {profileID: req.params.id, info: info});
 });
 
 app.get('/profile/:id', function (req, res) {

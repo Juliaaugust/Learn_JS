bef1ae4b80f7cbf45f155c8338001f6f7d0904cd
Julia <juliaaugust96@gmail.com>
2018-12-28 04:55:52 +0300

    Use ejs

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
+app.set("views", "./views");
+
 
 app.listen(3000);
 
-app.use('/css', express.static('style'))
+app.use('/css', express.static('css'));
+app.use('/img', express.static('img'));
+app.use('/js', express.static('js'));
+app.use('/lib', express.static('lib'));
+app.use('/mode', express.static('mode'));
+
+var info = {curr_level: "2", solved_tasks: "5", publ_articles: "0"};
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/main/index.html");
+  res.render('main');
 });
 
 app.get('/profile', function (req, res) {
-  var info = {curr_level: "2", solved_tasks: "5", publ_articles: "0"};
   res.render('profile', {profileID: req.params.id, info: info});
 });
 
 app.get('/profile/:id', function (req, res) {
-  var info = {curr_level: "2", solved_tasks: "5", publ_articles: "0"};
   res.render('profile', {profileID: req.params.id, info: info});
 });
+
+app.get('/task/:num', function (req, res) {
+  res.render('tasks/task', {profileID: req.params.id, taskNum: req.params.num});
+});
+
+app.get('/lesson/:num', function (req, res) {
+  res.render('lessons/lesson', {profileID: req.params.id, lessonNum: req.params.num});
+});
+
+// прописать отдельные получения кода (асинхронно отправляется код) POST
+// полученую строку с кодом и номер задачи запускать [eval()]
+
+// public: html, js, css, img
+// routes: файлы, которые отвечают за подпути (маршрутизация)
+
+// ajax

d25cbea0fb14c449970781ebb9cfb5872a08c979
Julia <juliaaugust96@gmail.com>
2019-04-21 23:04:21 +0300

    Activity calendar
    Use wiget "Activity calendar" in user profile

---
+++ js/calendar.js
@@ -0,0 +1,12 @@
+var calendar = new Calendar({
+  container: 'calendar-id',
+  color: '#964260',
+  data: [
+    ['2019-04-01', 2],
+    ['2019-04-02', 12],
+    ['2019-04-04', 8]
+  ],
+  intervals: [1, 3, 5, 8, 12],
+  num_weeks: 52,
+  day_size: 18
+});
